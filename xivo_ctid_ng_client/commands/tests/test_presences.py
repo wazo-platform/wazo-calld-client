5468e5a71e17737b2921b7b5a08e5f465612b989
Sylvain Boily <sylvainboilydroid@gmail.com>
2016-06-20 16:44:58 -0400

    add presences

---
+++ xivo_ctid_ng_client/commands/tests/test_presences.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2016 Avencall
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+from xivo_lib_rest_client.tests.command import RESTCommandTestCase
+
+from ..presences import PresencesCommand
+
+
+class TestPresences(RESTCommandTestCase):
+
+    Command = PresencesCommand
+
+    def test_update_presence(self):
+        self.session.put.return_value = self.new_response(204)
+
+        self.command.update_presence('user-uuid', 'available')
+
+        expected_body = {
+            'user_uuid': 'user-uuid',
+            'status_name': 'available',
+        }
+        self.session.put.assert_called_once_with(
+            self.base_url,
+            json=expected_body,
+            headers={'Accept': 'application/json',
+                     'Content-Type': 'application/json'})
+
+    def test_update_presence_from_user(self):
+        self.session.put.return_value = self.new_response(204)
+
+        self.command.update_presence_from_user('available')
+
+        expected_body = {
+            'status_name': 'available',
+        }
+        self.session.post.assert_called_once_with(
+            self.base_url,
+            json=expected_body,
+            headers={'Accept': 'application/json',
+                     'Content-Type': 'application/json'})
