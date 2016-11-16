aaf68415a90ee493f15bd4d5a99f9fbbd78d93f4
Pascal Cadotte Michaud <pcadotte@proformatique.com>
2016-11-16 13:35:38 -0500

    line presence: add the xivo_uuid param

--- xivo_ctid_ng_client/commands/line_presences.py
+++ xivo_ctid_ng_client/commands/line_presences.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2016 Avencall
+# Copyright (C) 2016 Proformatique, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -23,9 +24,13 @@ class LinePresencesCommand(RESTCommand):
     resource = 'presences'
     headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 
-    def get_presence(self, line_id):
-        r = self.session.get(self._client.url('lines', line_id, self.resource),
-                             headers=self.headers)
+    def get_presence(self, line_id, xivo_uuid=None):
+        kwargs = {'headers': self.headers}
+        if xivo_uuid:
+            params = {'xivo_uuid': xivo_uuid}
+            kwargs['params'] = params
+
+        r = self.session.get(self._client.url('lines', line_id, self.resource), **kwargs)
 
         if r.status_code != 200:
             self.raise_from_response(r)
--- xivo_ctid_ng_client/commands/tests/test_line_presences.py
+++ xivo_ctid_ng_client/commands/tests/test_line_presences.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Copyright (C) 2016 Avencall
+# Copyright (C) 2016 Proformatique, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,6 +17,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 from xivo_lib_rest_client.tests.command import RESTCommandTestCase
+
+from mock import sentinel as s
 
 from ..line_presences import LinePresencesCommand
 
@@ -35,3 +38,26 @@ class TestLinePresences(RESTCommandTestCase):
             self.client.url('lines', line_id, 'presences'),
             headers={'Accept': 'application/json',
                      'Content-Type': 'application/json'})
+
+    def test_get_presence_with_xivo_uuid(self):
+        self.session.get.return_value = self.new_response(200, dict())
+
+        self.command.get_presence(s.line_id, s.xivo_uuid)
+
+        self.session.get.assert_called_once_with(
+            self.client.url('lines', s.line_id, 'presences'),
+            headers={'Accept': 'application/json',
+                     'Content-Type': 'application/json'},
+            params={'xivo_uuid': s.xivo_uuid}
+        )
+
+    def test_get_presence_with_xivo_uuid_set_to_none(self):
+        self.session.get.return_value = self.new_response(200, dict())
+
+        self.command.get_presence(s.line_id, None)
+
+        self.session.get.assert_called_once_with(
+            self.client.url('lines', s.line_id, 'presences'),
+            headers={'Accept': 'application/json',
+                     'Content-Type': 'application/json'},
+        )
