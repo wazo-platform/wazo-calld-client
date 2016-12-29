100a23db2d4e41d36b330209e8fd73c221e9ac8b
Etienne Lessard <elessard97@gmail.com>
2016-12-29 14:36:22 -0500

    update for Wazo

--- setup.py
+++ setup.py
@@ -25,10 +25,10 @@ setup(
 
     description='a simple client library for the xivo-ctid-ng HTTP interface',
 
-    author='Avencall',
-    author_email='dev@avencall.com',
+    author='Wazo Authors',
+    author_email='dev.wazo@gmail.com',
 
-    url='https://github.com/wazo-pbx/xivo-ctid-ng-client',
+    url='http://wazo.community',
 
     packages=find_packages(),
 
