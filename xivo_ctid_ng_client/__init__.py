f7394746a5a2d0db0f140979619822a83928fc71
Francois Blackburn <fblackburn@wazo.community>
2019-02-22 15:00:07 -0500

    update SPDX-License-Identifier

--- xivo_ctid_ng_client/__init__.py
+++ xivo_ctid_ng_client/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2015 Avencall
-# SPDX-License-Identifier: GPL-3.0+
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_ctid_ng_client.client import CtidNGClient as Client
