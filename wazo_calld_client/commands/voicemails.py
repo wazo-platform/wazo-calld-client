31cc33d127dc81d337c57a7cf14963285c615345
Mehdi Abaakouk <mabaakouk@wazo.io>
2019-07-01 13:01:59 +0200

    Add voicemails greetings API

--- wazo_calld_client/commands/voicemails.py
+++ wazo_calld_client/commands/voicemails.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_lib_rest_client import RESTCommand
@@ -64,11 +64,47 @@ class VoicemailsCommand(RESTCommand):
         url = self._client.url('users', 'me', 'voicemails', 'messages', message_id, 'recording')
         return self._get_recording(url)
 
+    def get_voicemail_greeting(self, voicemail_id, greeting):
+        url = '{url}/{voicemail_id}/greetings/{greeting}'.format(url=self.base_url, voicemail_id=voicemail_id, greeting=greeting)
+        return self._get_recording(url)
+
+    def get_voicemail_greeting_from_user(self, greeting):
+        url = self._client.url('users', 'me', 'voicemails', 'greetings', greeting)
+        return self._get_recording(url)
+
+    def update_voicemail_greeting(self, voicemail_id, greeting, data):
+        url = '{url}/{voicemail_id}/greetings/{greeting}'.format(url=self.base_url, voicemail_id=voicemail_id, greeting=greeting)
+        return self._put_recording(url, data)
+
+    def update_voicemail_greeting_from_user(self, greeting, data):
+        url = self._client.url('users', 'me', 'voicemails', 'greetings', greeting)
+        return self._put_recording(url, data)
+
+    def delete_voicemail_greeting(self, voicemail_id, greeting):
+        url = '{url}/{voicemail_id}/greetings/{greeting}'.format(url=self.base_url, voicemail_id=voicemail_id, greeting=greeting)
+        self.session.delete(url)
+
+    def delete_voicemail_greeting_from_user(self, greeting):
+        url = self._client.url('users', 'me', 'voicemails', 'greetings', greeting)
+        self.session.delete(url)
+
+    def copy_voicemail_greeting(self, voicemail_id, greeting, dest_greeting):
+        url = '{url}/{voicemail_id}/greetings/{greeting}/copy'.format(url=self.base_url, voicemail_id=voicemail_id, greeting=greeting)
+        self.session.post(url, json={'dest_greeting', dest_greeting})
+
+    def copy_voicemail_greeting_from_user(self, greeting, dest_greeting):
+        url = self._client.url('users', 'me', 'voicemails', 'greetings',
+                               greeting, 'copy')
+        self.session.post(url, json={'dest_greeting', dest_greeting})
+
+    def _put_recording(self, url, data):
+        r = self.session.put(url, headers={'Content-type': 'audio/wav'},
+                             data=data)
+        self.raise_from_response(r)
+
     def _get_recording(self, url):
         r = self.session.get(url, headers={'Accept': 'audio/wav'})
-        if r.status_code != 200:
-            self.raise_from_response(r)
-
+        self.raise_from_response(r)
         return r.content
 
     def _get(self, url):
