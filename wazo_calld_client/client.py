9ebb777d69493156dc0d404d13dfb1b2e44612e7
Francois Blackburn <fblackburn@wazo.community>
2019-10-13 16:16:09 -0400

    use full name for entry points

--- wazo_calld_client/client.py
+++ wazo_calld_client/client.py
@@ -7,7 +7,7 @@ from wazo_lib_rest_client.client import BaseClient
 
 class CalldClient(BaseClient):
 
-    namespace = 'calld_client.commands'
+    namespace = 'wazo_calld_client.commands'
 
     def __init__(self, host, port=9500, version='1.0', **kwargs):
         super(CalldClient, self).__init__(
