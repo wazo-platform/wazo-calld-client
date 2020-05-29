b40735da602476ec92ea51e0ba5ec46f9dcadb80
Francois Blackburn <fblackburn@wazo.community>
2020-05-29 06:58:06 -0400

    use prefix on port 443 by default

--- wazo_calld_client/client.py
+++ wazo_calld_client/client.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_lib_rest_client.client import BaseClient
@@ -9,7 +9,7 @@ class CalldClient(BaseClient):
 
     namespace = 'wazo_calld_client.commands'
 
-    def __init__(self, host, port=9500, version='1.0', **kwargs):
+    def __init__(self, host, port=443, prefix='/api/calld', version='1.0', **kwargs):
         super(CalldClient, self).__init__(
-            host=host, port=port, version=version, **kwargs
+            host=host, port=port, prefix=prefix, version=version, **kwargs
         )
