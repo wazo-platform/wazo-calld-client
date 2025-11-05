7cb7fcd0ba59cdc0193ba2a58464a7c3fbdb039e
Julien Alie <jalie@wazo.io>
2025-11-05 15:40:02 -0500

    allow passing listing parameters to voicemail messages endpoint
    supports: order, direction, limit and offset

--- wazo_calld_client/commands/voicemails.py
+++ wazo_calld_client/commands/voicemails.py
@@ -32,9 +32,9 @@ class VoicemailsCommand(CalldCommand):
         url = self._client.url('users', 'me', 'voicemails', 'messages', message_id)
         return self._get(url)
 
-    def get_all_voicemail_messages_from_user(self, voicemail_type='all'):
+    def get_all_voicemail_messages_from_user(self, voicemail_type='all', **params):
         url = self._client.url('users', 'me', 'voicemails', 'messages')
-        return self._get(url, {"voicemail_type": voicemail_type})
+        return self._get(url, {"voicemail_type": voicemail_type} | params)
 
     def delete_voicemail_message(self, voicemail_id, message_id):
         headers = self._get_headers()
